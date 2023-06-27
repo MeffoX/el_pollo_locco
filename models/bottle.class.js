8f17af4e15112a2ef26a4e468030fb74188387ae
Stefan <roth.neukirchen@googlemail.com>
2023-06-27 13:06:29 +0200

    add Starting BG

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,10 +1,7 @@
 class Bottle extends ThrowableObject {
 
     
-    IMAGES_GROUND = [
-        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
-        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
-    ]
+
 
 
     constructor() {
--- models/throwable-objects.class.js
+++ models/throwable-objects.class.js
@@ -1,5 +1,9 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_GROUND = [
+        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
+    ]
 
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
